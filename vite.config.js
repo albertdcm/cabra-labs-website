dd88bfcc7d51aa929401723c5732d1d96e0fec25
albertdcm <albertdcm2828@gmail.com>
2025-12-11 03:36:15 +0000

    fix: configurar root correcto de Vite

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  // La carpeta real del proyecto Vite
+  root: "cabra-labs-website",
+
   // Como usas dominio propio (cabralabs.com), la base es la raíz
   base: "/",
-});
 
+  // Dónde queremos que salga el build final
+  build: {
+    outDir: "../dist",   // genera /dist en la raíz del repo
+    emptyOutDir: true,   // limpia la carpeta antes de generar
+  },
+});
 
