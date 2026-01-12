d0fa7a8aabfd9211a3f32dfa98c3ef14fffaf61b
albertdcm <albertdcm2828@gmail.com>
2026-01-12 09:57:03 +0000

    fix: use relative links for static pages and build

--- docs/assets/main-DXZ9bH2d.js
+++ docs/assets/main-DXZ9bH2d.js
@@ -832,8 +832,8 @@ import{C as c,T as d,I as n}from"./Cabra Labs Logo--6aE7PCr.js";const p="/assets
                   Contacto
                 </a>
               </li>
-              <li><a class="hover:text-emerald-200 transition-colors" href="/faq.html">Preguntas frecuentes</a></li>
-              <li><a class="hover:text-emerald-200 transition-colors" href="/privacy.html">Política de Privacidad</a></li>
+              <li><a class="hover:text-emerald-200 transition-colors" href.="/faq.html">Preguntas frecuentes</a></li>
+              <li><a class="hover:text-emerald-200 transition-colors" href=."/privacy.html">Política de Privacidad</a></li>
               <li><a class="hover:text-emerald-200 transition-colors" href="/terms.html">Términos y Condiciones</a></li>
             </ul>
           </div>
